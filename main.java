e9ca4665eb079002cf1fd637dc8dffcedee3ed66
turculetandrei <andreit8585@gmail.com>
2025-03-14 12:13:14 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,12 @@
 
+public class Main {
+    public static void main(String[] args) {
+    	
+    	
+        Sertar s1=new Sertar(40, 50, 20);
+        Sertar s2=new Sertar(45, 55, 25);
+        Birou birou=new Birou(120, 60, 75, s1, s2);
+
+        birou.tipareste();
+    }
+}
