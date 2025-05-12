2e583ebe5432aba918501939f03f3c4682022e53
turculetandrei <andreit8585@gmail.com>
2025-05-12 18:09:28 +0300

    Update script.js

--- tema11/script.js
+++ tema11/script.js
@@ -1,20 +1,35 @@
-let schimbareCulori;
+let culoare1 = 'red';
+let culoare2 = 'blue';
+let schimbare = false;
+let ultimulTimp = 0;
 
-function porneste() {
-    schimbareCulori = setInterval(() => {
-        let zona1 = document.getElementById("zona1");
-        let zona2 = document.getElementById("zona2");
-
-        if (zona1.style.backgroundColor === "red") {
-            zona1.style.backgroundColor = "blue";
-            zona2.style.backgroundColor = "red";
-        } else {
-            zona1.style.backgroundColor = "red";
-            zona2.style.backgroundColor = "blue";
-        }
-    }, 1000);
+function setup() {
+  createCanvas(220, 100);
+  rectMode(CORNER);
 }
 
-function opreste() {
-    clearInterval(schimbareCulori);
+function draw() {
+  background(220);
+
+  fill(culoare1);
+  rect(10, 25, 100, 50);
+
+  fill(culoare2);
+  rect(110, 25, 100, 50);
+
+  if (schimbare && millis() - ultimulTimp >= 1000) {
+    let aux = culoare1;
+    culoare1 = culoare2;
+    culoare2 = aux;
+    ultimulTimp = millis();
+  }
+}
+
+function keyPressed() {
+  if (key === 's' || key === 'S') {
+    schimbare = true;
+    ultimulTimp = millis();
+  } else if (key === 'x' || key === 'X') {
+    schimbare = false;
+  }
 }
